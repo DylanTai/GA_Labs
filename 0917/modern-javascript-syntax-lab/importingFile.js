f41ce20ac20d1d853b099809ebbda9a943813af2
Dylan Tai <dylztai@gmail.com>
2025-09-18 08:53:05 -0400

    Finished modern-javascript-syntax-lab

---
+++ 0917/modern-javascript-syntax-lab/importingFile.js
@@ -0,0 +1,5 @@
+import name, { computer, age, job } from "./exportingFile.js";
+
+console.log("Exercise 7:");
+console.log(name, computer, age, job);
+// Prints: 'Matt', 'MacBook Pro', 43, 'programmer'
