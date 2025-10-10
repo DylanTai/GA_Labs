452062d8f8d157d21d49c2958906c88ec44279de
Dylan Tai <dylztai@gmail.com>
2025-10-10 09:09:37 -0400

    fixed ex9 to be better

--- 1009/python-functions-lab/exercises.py
+++ 1009/python-functions-lab/exercises.py
@@ -194,18 +194,16 @@ print()
 # Define the function and then call it below.
 
 def basic_calculator(num1, num2, operation):
+    result = 0
     if operation == 'add':
         result = num1 + num2
-        return result
     elif operation == 'subtract':
         result = num1 - num2
-        return result
     elif operation == 'multiply':
         result = num1 * num2
-        return result
     elif operation == 'divide':
         result = num1 / num2
-        return result
+    return result
 
 print('Exercise 9:')
 print("basic_calculator(10, 5, 'subtract') =", basic_calculator(10, 5, "subtract"))
